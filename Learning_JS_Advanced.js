e456f344653ef6b0641b6fafef5b5351fe60c809
AnirudhGirish <anirudhgirish08@gmail.com>
2024-11-26 17:50:19 +0530

    Added advanced concepts as Hoisting and DOM manupalation

--- Learning_JS_Advanced.js
+++ Learning_JS_Advanced.js
@@ -1,3 +1,18 @@
 //! The Way I learnt JS 
 // Advanced
 
+//Hoisting it a JS concept where variables can be used before they are inatialised and also can be used in higher scope.
+// Hoisting Example 1
+function codeHoist() {
+    a = 10;
+    let b = 50;
+}
+codeHoist();
+console.log(a); // 10
+console.log(b); // ReferenceError : b is not defined
+
+// Hoisting Example 2
+fun(); // Calling before declaration
+function fun() { // Declaring
+  console.log("Function is hoisted");
+}
